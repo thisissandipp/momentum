8df97a1db554aeed7b2c350def837358da9f4a93
Sandip <sandipramanik07@gmail.com>
2025-06-29 20:26:48 +0530

    feat: add backend to generate habits using google ai sdk

---
+++ app/api/habit-architect/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse, type NextRequest } from 'next/server';
+import { habitArchitectPrompt } from '@/lib/prompts';
+import { currentUser } from '@/lib/current-user';
+import { google } from '@ai-sdk/google';
+import { domains } from '@/lib/domains';
+import { habitSchema } from '@/types';
+import { generateObject } from 'ai';
+import { z } from 'zod';
+
+const bodySchema = z.object({
+  domain: z.enum(domains),
+  oneYearGoal: z.string().min(15, 'Minimum 15 charaters are required'),
+  currentState: z.string().min(15, 'Minimum 15 charaters are required'),
+});
+
+export async function POST(req: NextRequest) {
+  const user = await currentUser();
+  if (!user) {
+    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+  }
+
+  const body = await req.json();
+  const parsed = bodySchema.safeParse(body);
+
+  if (!parsed.success) {
+    return NextResponse.json({ message: parsed.error.message }, { status: 400 });
+  }
+
+  const { domain, oneYearGoal, currentState } = parsed.data;
+
+  const result = await generateObject({
+    model: google('gemini-2.0-flash'),
+    schema: z.array(habitSchema),
+    prompt: habitArchitectPrompt(domain, oneYearGoal, currentState),
+  });
+
+  return NextResponse.json({ habits: result.object }, { status: 200 });
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,16 @@
 'use client';
 
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { useEffect, useState } from 'react';
+import type { Habit } from '@/types';
 import { User } from '@/db/types';
 import axios from 'axios';
 
 export default function DashboardPage() {
   const [user, setUser] = useState<User | null>(null);
+  const [habits, setHabits] = useState<Habit[]>([]);
 
   useEffect(() => {
     const currentUser = async () => {
@@ -17,14 +22,48 @@ export default function DashboardPage() {
       }
     };
 
+    const generateHabitSuggestions = async () => {
+      const response = await axios.post('/api/habit-architect', {
+        domain: 'Contribution & Purpose',
+        oneYearGoal: 'Volunteer 4 times at the local shelter by year-end',
+        currentState: "I haven't volunteered before, but I care about working with animals.",
+      });
+      console.log(response.data.habits);
+      setHabits(response.data.habits satisfies Habit[]);
+    };
+
     currentUser();
+    generateHabitSuggestions();
   }, []);
 
   return (
-    <div className="flex justify-center py-56 md:py-64 lg:py-72">
-      <h2 className="text-center text-lg font-semibold">
-        Welcome to dashboard, {user ? user.displayName.split(' ')[0] : 'boss'}!
-      </h2>
+    <div className="mx-8 py-24 md:py-32 lg:py-48">
+      <div className="mx-auto mt-10 grid max-w-6xl place-items-center md:grid-cols-2 md:gap-24">
+        <div>
+          <h2 className="text-lg font-semibold">
+            Welcome to Intelligent Habit Architect, {user ? user.displayName.split(' ')[0] : 'boss'}
+            !
+          </h2>
+          <Input type="text" placeholder="Domain for your goal" className="mt-8" />
+          <Textarea placeholder="What do you want to achieve in one year?" className="mt-8" />
+          <Textarea
+            placeholder="How are you doing currently to achieve what you want in one year?"
+            className="mt-8"
+          />
+          <Button className="mt-8" variant="secondary">
+            Generate Habit Suggestions
+          </Button>
+        </div>
+        <div>
+          {habits.map((habit) => (
+            <div key={habit.habit} className="mx-auto mt-8 flex max-w-3xl flex-row items-start">
+              <h3 className="text-muted-foreground ml-4 text-xs text-balance sm:text-base/8">
+                {habit.habit}
+              </h3>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
